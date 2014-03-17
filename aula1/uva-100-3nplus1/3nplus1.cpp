d2766a4c783fa4928eeb30419f2e5f322e3c21c2
Gabriel Trabasso <gabrieljt@gmail.com>
2014-03-16 23:32:35 -0300

    UVA 3nplus1 problem accepted

--- aula1/uva-100-3nplus1/3nplus1.cpp
+++ aula1/uva-100-3nplus1/3nplus1.cpp
@@ -1,8 +1,45 @@
 // Gabriel Trabasso - 298573
 // 3n + 1 problem - UVA - 100
 
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
 int main()
 {
+    auto i = 0u;
+    auto j = 0u;
+    std::vector<unsigned int> cycles;
+
+    while (scanf("%u %u", &i, &j) > 0)
+    {   
+        auto swapped = false;
+        if (i > j)
+        {
+            auto tmp = i;
+            i = j;
+            j = tmp;
+            swapped = true;
+        }
+
+        for (auto n = i; n <= j; ++n)
+        {
+            auto m = n;
+            auto cycle = 1u; // The number itself
+            while (m != 1u)
+            {
+                if (m % 2u == 0u)
+                    m /= 2u;
+                else
+                    m = 3u*m + 1u;
+                ++cycle;
+            }
+            cycles.push_back(cycle);
+        }       
+        auto maxCycle = std::max_element(cycles.begin(), cycles.end()); // Returns Iterator
+        swapped ? printf("%u %u %u\n", j, i, *maxCycle) : printf("%u %u %u\n", i, j, *maxCycle);
+        cycles.clear();
+    }
 
     return 0;
 }
